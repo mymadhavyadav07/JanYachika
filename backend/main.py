8f7328f4175df89f53c4f55fd80b87111526f0f1
Madhav Y. <66372332+mymadhavyadav07@users.noreply.github.com>
2025-09-24 22:22:50 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origin_list, # frontend origin 
+    allow_origins=["https://janyachika.vercel.app"], # frontend origin 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
