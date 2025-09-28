0c70aa6dcbc572d44608d03c5c421218e7980078
mymadhavyadav07 <mymadhavyadav07@gmail.com>
2025-09-28 10:07:01 +0530

    Fixed Logout Issue

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,7 @@ async def options_handler():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://janyachika.vercel.app"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=[
